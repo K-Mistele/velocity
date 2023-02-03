bbee428b91811495b699ed4084526c131af50340
Kyle Mistele <kyle@mistele.com>
2023-02-03 13:14:36 -0600

    add windowed application

--- velocity-cli/velocity-cli.cpp
+++ velocity-cli/velocity-cli.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <Windows.h>
+#include <Shellapi.h>
 #include <tchar.h>
 #include <Psapi.h>
 #include <shlwapi.h>
@@ -10,6 +11,9 @@
 
 HHOOK hHook = NULL;
 KBDLLHOOKSTRUCT kbdStruct;
+
+
+
 
 LRESULT __stdcall KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -39,7 +43,7 @@ LRESULT __stdcall KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
             // Compare the application name, and skip execution if the image is Linear or Notion
             if (_tcscmp(_T("Linear.exe"), executableName) == 0 || _tcscmp(_T("Notion.exe"), executableName) == 0 ||  _tcscmp(_T("brave.exe"), executableName) == 0)
             {
-
+                
                 std::cout << "Ctrl + K pressed (no action)" << std::endl;
                 std::wcout << "Image name: " << executableName << std::endl;
                 // Return without taking any action if the active window is OtherApp
@@ -85,10 +89,44 @@ LRESULT __stdcall KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 }
 
 
-int main()
-{
-    std::cout << "Hello World!\n";
 
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+// Global Variables
+NOTIFYICONDATA nid;
+HWND hwnd;
+
+// Function Declarations
+void InitNotifyIconData();
+void RegisterClass();
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    // Register the window class
+    RegisterClass();
+
+    // Create the window
+    hwnd = CreateWindowEx(0, L"MyWindowClass", L"My Window", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, NULL, hInstance, NULL);
+
+    // Initialize the NOTIFYICONDATA structure
+    InitNotifyIconData();
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
     // Set the hook
     hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
 
@@ -108,18 +146,75 @@ int main()
 
     }
 
+
     return 0;
+
 }
 
+// Function Definitions
+void InitNotifyIconData()
+{
+    nid.cbSize = sizeof(NOTIFYICONDATA);
+    nid.hWnd = hwnd;
+    nid.uID = 100;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_USER + 1;
+    nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcscpy_s(nid.szTip, L"My App");
+    Shell_NotifyIcon(NIM_ADD, &nid);
+}
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void RegisterClass()
+{
+    WNDCLASSEX wc;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = L"MyWindowClass";
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBoxA(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        exit(EXIT_FAILURE);
+    }
+}
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_USER + 1:
+        switch (lParam)
+        {
+        case WM_LBUTTONDOWN:
+            ShowWindow(hwnd, SW_RESTORE);
+            break;
+        case WM_RBUTTONDOWN:
+            // Show context menu here
+            break;
+        }
+        break;
+    case WM_SIZE:
+        if (wParam == SIZE_MINIMIZED)
+        {
+            ShowWindow(hwnd, SW_HIDE);
+        }
+        break;
+    case WM_DESTROY:
+        Shell_NotifyIcon(NIM_DELETE, &nid);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+    return 0;
+}
